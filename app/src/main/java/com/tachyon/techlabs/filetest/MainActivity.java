d18f2137ca032fa62111786fb46bdd67a0942d1b
Tachyon Tech Labs <pranjalpchaudhari99@gmail.com>
2018-12-05 23:27:37 +0530

    pranjal 2

--- app/src/main/java/com/tachyon/techlabs/filetest/MainActivity.java
+++ app/src/main/java/com/tachyon/techlabs/filetest/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
 
-     //tachyon2
+     //pranjal2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
