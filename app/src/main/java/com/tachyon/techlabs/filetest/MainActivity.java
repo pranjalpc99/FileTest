bf5eb181e5c8ae6370a00f81a0de03cb160bd540
Tachyon Tech Labs <tachyontechlabs@gmail.com>
2018-12-06 11:12:57 +0530

    took pc commmit

--- app/src/main/java/com/tachyon/techlabs/filetest/MainActivity.java
+++ app/src/main/java/com/tachyon/techlabs/filetest/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //testing 
+        //testing
 
         mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
             @Override
