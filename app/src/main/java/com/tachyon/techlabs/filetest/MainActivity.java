50ab15a48e00cdd4e3e26ea3e0c4911784222ea0
Tachyon Tech Labs <tachyontechlabs@gmail.com>
2018-12-06 10:35:47 +0530

    pranjal last

--- app/src/main/java/com/tachyon/techlabs/filetest/MainActivity.java
+++ app/src/main/java/com/tachyon/techlabs/filetest/MainActivity.java
@@ -97,8 +97,12 @@ public class MainActivity extends AppCompatActivity {
         return Environment.MEDIA_MOUNTED.equals(state);
     }
 
+    //pranjal
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        //extra pranjal
 
         if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
             Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
